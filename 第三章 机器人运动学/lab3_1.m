d51b21ecf057b121bd33f09a50fd6869700f45b7
Sihan0229 <1109458800@qq.com>
2024-10-15 00:33:58 +0800

    lab31加注释

--- 第三章 机器人运动学/lab3_1.m
+++ 第三章 机器人运动学/lab3_1.m
@@ -1,6 +1,7 @@
 % 实验3.1 p79-81
 %%
 % 建立关节机器人模型1
+% 使用DH参数来定义每个关节
 L(1) = Revolute('d', 0, 'a', 0, 'alpha', pi/2);
 L(2) = Revolute('d', 0, 'a', 0.43, 'alpha', 0);
 L(3) = Revolute('d', 0.15, 'a', 0.02, 'alpha', -pi/2);
@@ -9,7 +10,7 @@ L(5) = Revolute('d', 0, 'a', 0, 'alpha', -pi/2);
 L(6) = Revolute('d', 0, 'a', 0, 'alpha', 0);
 robot = SerialLink(L, 'name', 'PUMA560');
 view(3);
-robot.plot([0 0 0 0 0 0]);
+robot.plot([0 0 0 0 0 0]); % 绘制机器人在6个关节都为0角度时的姿态。
 
 %%
 % 建立关节机器人模型2
@@ -25,12 +26,12 @@ robot.plot([0 0 0 0 0 0])
 
 %%
 % 求解正运动学
-theta = [pi/2 0 pi/2 0 0 0];
-T = robot.fkine(theta);
+theta = [pi/2 0 pi/2 0 0 0]; % 定义6个关节的角度配置，单位是弧度。
+T = robot.fkine(theta); % 计算给定关节角度下的末端执行器的齐次变换矩阵T，描述末端执行器的位姿（位置和方向）。
 
 %%
 % 求解逆运动学
-targetPos = [-0.06 -0.2 0.1];
-targetform = rpy2tr(90, -180, -180);
+targetPos = [-0.06 -0.2 0.1]; % 定义目标末端执行器的位置，即[x, y, z]坐标。
+targetform = rpy2tr(90, -180, -180); % rpy2tr函数将Roll-Pitch-Yaw角转换为齐次变换矩阵形式
 TR = transl(targetPos)*targetform;
 q = robot.ikine(TR);
