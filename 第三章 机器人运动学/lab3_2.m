afe5eda4304fcfd87bf2d6b9563f3fdf03b2caf0
Sihan0229 <1109458800@qq.com>
2024-10-14 15:58:04 +0800

    lab3.2

---
+++ 第三章 机器人运动学/lab3_2.m
@@ -0,0 +1,80 @@
+% 实验3.2 p81-83
+mdl_puma560;
+
+%%
+% 计算过程
+T1 = transl(0, -0.25, 0) * trotx(pi/2);
+T2 = transl(0.5, 0.25, 0.5) * trotx(pi/2);
+q1 = p560.ikine(T1);
+q2 = p560.ikine(T2);
+t = [0:0.05:2]';
+q = jtraj(q1, q2, t);
+p560.plot(q);
+figure;
+
+%%
+% 计算关节角度变化
+for i = 1:6
+    subplot(3, 2, i);   % 创建 3x2 的布局，每次在第 i 个子图上绘制
+    plot(t, q(:, i));   % 绘制第 i 列的 q 数据相对于 
+    xlabel('Time/s');  % x 轴标签
+    ylabel(['q' num2str(i) '/rad']);  % y 轴标签，显示 q 的列号
+end
+
+% 调整图像布局，使子图之间的间距更合理
+tightfig;
+
+%%
+% 计算末端执行器的位置
+ee_position = zeros(length(t), 3);  % 初始化末端执行器位置矩阵
+for i = 1:length(t)
+    T = p560.fkine(q(i, :));  % 通过正向运动学计算末端执行器的位姿
+    ee_position(i, :) = transl(T);  % 提取末端执行器的平移部分（位置）
+end
+
+% 绘制末端执行器的运动轨迹
+figure;
+plot3(ee_position(:, 1), ee_position(:, 2), ee_position(:, 3), 'LineWidth', 2);
+grid on;
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+title('End Effector Trajectory');
+
+%%
+% 笛卡尔坐标轨迹规划
+T1 = transl(0, -0.25, 0) * trotx(pi/2);
+T2 = transl(0.5, 0.25, 0.5) * trotx(pi/2);
+t = [0:0.05:2]';
+Ts = ctraj(T1,T2,length(t));
+q = p560.ikine6s(Ts);
+p560.plot(q);
+
+%%
+% 计算关节角度变化
+for i = 1:6
+    subplot(3, 2, i);   % 创建 3x2 的布局，每次在第 i 个子图上绘制
+    plot(t, q(:, i));   % 绘制第 i 列的 q 数据相对于 
+    xlabel('Time/s');  % x 轴标签
+    ylabel(['q' num2str(i) '/rad']);  % y 轴标签，显示 q 的列号
+end
+
+% 调整图像布局，使子图之间的间距更合理
+tightfig;
+
+%%
+% 计算末端执行器的位置
+ee_position = zeros(length(t), 3);  % 初始化末端执行器位置矩阵
+for i = 1:length(t)
+    T1 = p560.fkine(q(i, :));  % 通过正向运动学计算末端执行器的位姿
+    ee_position(i, :) = transl(T1);  % 提取末端执行器的平移部分（位置）
+end
+
+% 绘制末端执行器的运动轨迹
+figure;
+plot3(ee_position(:, 1), ee_position(:, 2), ee_position(:, 3), 'LineWidth', 2);
+grid on;
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+title('End Effector Trajectory');
